aba61668a5897a5a57aa9fc07c60771a7f74709e
Hasnan <monirhasnan@gmail.com>
2023-06-04 23:57:18 +0600

    dashboard delete added

--- src/Pages/Dashboard/MyCart.jsx
+++ src/Pages/Dashboard/MyCart.jsx
@@ -1,17 +1,91 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../hooks/useCart";
+import { Link } from "react-router-dom";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
-  const [cart] = useCart();
+  const [cart, refetch] = useCart();
   console.log(cart);
   const total = cart.reduce((sum, item) => item.price + sum, 0);
   console.log(total);
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${item._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
   return (
-    <div>
+    <div className="min-h-screen mt-[25px]">
       <Helmet>
         <title>Bistro Boss | My Cart</title>
       </Helmet>
-      <h2>My Cart</h2>
+      <div className="flex justify-between items-center bg-purple-700 text-white p-5 rounded-md mb-[25px]">
+        <h2>Total items: {cart.length}</h2>
+        <h2>Price: ${total}</h2>
+        <Link className="btn" to="payment">
+          Go to Payment
+        </Link>
+      </div>
+      <div className="">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td className="text-end">${item.price}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(item)}
+                    className="btn btn-ghost bg-red-600  text-white"
+                  >
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
